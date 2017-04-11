89034f474fcdff99bff4a46cd65e2caf30860c7b
Sakshi Gupta <sakshi95gupta@gmail.com>
2017-04-11 16:12:37 +0530

    Add files via upload

---
+++ Project-5.py
@@ -0,0 +1,11 @@
+list={'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayinventory(l):
+    total = 0
+    print "Inventory:"
+    for a,b in l.items():
+        print b,a
+        total=total+b
+    print "total number of items ",total
+
+displayinventory(list)
